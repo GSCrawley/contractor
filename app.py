bbcafe500b08a4951d5f821d9cab4883148674c6
GSCrawley <gscrawley@gmail.com>
2019-10-06 15:39:06 -0700

    installed pymongo

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+import os
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from flask import Flask, render_template, request, redirect, url_for
 
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+guitars = db.guitars
+# comments = db.comments
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+def guitars_index():
+    """Show all guitars."""
+    return render_template('guitars_index.html', guitars=guitars)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+# @app.route('/guitars/new')
+# def guitars_new():
+#     """Create a custom guitar store."""
+#     return render_template('guitars_new.html', guitar={}, title='Custom Guitars')
+#
+# @app.route()
